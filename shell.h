6d6cd31ee5dd2a474d1a606cbb2f0a8082df3d0a
YinhlaNdalo <nkatyndobe@gmail.com>
2023-08-22 11:32:08 -0700

    shell

--- shell.h
+++ shell.h
@@ -2,14 +2,14 @@
 #define SHELL_H
 
 #include <stdio.h>
-#Include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
-#include unstd.h>
+#include <unstd.h>
 #include <stdbool.h>
 
-void _prompt(void);promp
-void _print (const char* commands);
-void user_input(char*commands, size_t size);
+void _prompt(void);
+void _print (const char *commands);
+void user_input(char *commands, size_t size);
 void exec_command(const char *commands);
 
 #endif
